db577cd95de2918b584d25210ab6af0599c2026d
IlikeChooros <pawel.smolnicki@gmail.com>
2024-03-03 09:59:30 +0100

    Update asset paths and error page layout

--- web/src/pages/Error.js
+++ web/src/pages/Error.js
@@ -5,10 +5,11 @@ import ErrorIcon from '@mui/icons-material/Error';
 export function ErrorPage(){
     return (
         <MainFramework>
-            <Box padding={6} textAlign={'center'}>
-                <Typography variant="h5">404. Nie znaleziono strony</Typography>
-                <Typography variant="h6">Wróć do <Link href="/">strony głównej</Link></Typography>
-                <ErrorIcon sx={{fontSize: 100, color: 'red', marginTop: 2}} />
+            <Box textAlign={'center'} marginTop={'50%'}>
+                <Typography variant="h5">Błąd 404</Typography>
+                <Typography variant="h4">Nie znaleziono strony</Typography>
+                <Typography variant='h6'>Wróć do <Link href="/">strony głównej</Link></Typography>
+                <ErrorIcon sx={{fontSize: 100, color: 'crimson', marginTop: 2}} />
             </Box>
         </MainFramework>
     )
