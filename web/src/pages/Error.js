f4f47334e8ee68ac91539dd0fa3761501d57b124
IlikeChooros <pawel.smolnicki@gmail.com>
2024-03-03 09:12:10 +0100

    update error page

--- web/src/pages/Error.js
+++ web/src/pages/Error.js
@@ -5,7 +5,7 @@ import ErrorIcon from '@mui/icons-material/Error';
 export function ErrorPage(){
     return (
         <MainFramework>
-            <Box textAlign={'center'} marginTop={'50%'}>
+            <Box textAlign={'center'} marginTop={'10%'}>
                 <Typography variant="h5">Błąd 404</Typography>
                 <Typography variant="h4">Nie znaleziono strony</Typography>
                 <Typography variant='h6'>Wróć do <Link href="/">strony głównej</Link></Typography>
